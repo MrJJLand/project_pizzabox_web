563b9318d1ff3760e391874947901e0bcc4c600f
Jacob Polivka <jacob.polivka@revature.net>
2021-05-03 13:15:07 -0400

    Initial client-branch commit

---
+++ PizzaBox.Client/Models/OrderViewModel.cs
@@ -0,0 +1,15 @@
+
+
+namespace PizzaBox.Client.Models
+{
+    public class OrderViewModel
+    {
+        public List<string> Crusts { get; set; } = new List<string> { "Original", "Stuffed Crust", "Pretzel" };
+        public List<string> Sizes { get; set; }
+        public List<string> Toppings { get; set; }
+        public string SelectedCrust { get; set; }
+        public string SelectedSize { get; set; }
+        public List<string> SelectedToppings { get; set; }
+
+    }
+}
